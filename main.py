e9897af3877ca5963e1452f46c2164fad385b020
TRISHA <trishaverma0192@gmail.com>
2026-01-21 22:37:27 +0530

    Lab 1 Assignment

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Lab Assignment 1: Employee Salary Program
+name = input("Enter Employee Name: ")
+emp_id = input("Enter Employee ID: ")
+department = input("Enter Department: ")
+basic_salary = float(input("Enter Basic Salary: "))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+da = 0.92 * basic_salary
+hra = 0.58 * basic_salary
+ta = 0.30 * basic_salary
+lic = 500  # Fixed deduction
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+gross_salary = basic_salary + da + hra + ta
+net_salary = gross_salary - lic
+
+print("\n--- Employee Salary Details ---")
+print("Name:", name)
+print("Employee ID:", emp_id)
+print("Department:", department)
+print("Basic Salary:", basic_salary)
+print("DA (92%):", da)
+print("HRA (58%):", hra)
+print("TA (30%):", ta)
+print("LIC Deduction:", lic)
+print("Gross Salary:", gross_salary)
+print("Net Salary:", net_salary)
